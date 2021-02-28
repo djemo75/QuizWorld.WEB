73c27fe4c73851c4e42d7b348a5f51c15b5c9e45
Djem Suleyman <djem.suleyman@ad.mentormate.bg>
2021-02-28 18:07:22 +0200

    add environment variable for API URL

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:5000/api/";
+const baseURL = process.env.REACT_APP_API_URL;
 
 const axiosSetup = () => {
   axios.interceptors.request.use((request) => {
